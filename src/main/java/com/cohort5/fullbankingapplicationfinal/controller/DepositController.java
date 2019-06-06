7c8f69aa3bf0a77da1de1f3d0b78593bd659cb9f
christopherEdge <edgetopher@gmail.com>
2019-06-06 13:48:25 -0400

    Deposit service and controller

--- src/main/java/com/cohort5/fullbankingapplicationfinal/controller/DepositController.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/controller/DepositController.java
@@ -1,4 +1,36 @@
 package com.cohort5.fullbankingapplicationfinal.controller;
 
+import com.cohort5.fullbankingapplicationfinal.model.Deposit;
+import com.cohort5.fullbankingapplicationfinal.repository.DepositRepository;
+import com.cohort5.fullbankingapplicationfinal.service.DepositService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@RestController
 public class DepositController {
+    @Autowired
+    public DepositService depositService;
+
+    @RequestMapping(value = "/accounts/{accountId}/deposits", method = RequestMethod.POST)
+    public Deposit createDeposit(@PathVariable Long account_Id, Deposit deposit){
+        depositService.createDeposit(account_Id,deposit);
+        return depositService.getDepositById(deposit.getId());
+    }
+
+    @RequestMapping(value = "/deposits/{depositId}", method = RequestMethod.GET)
+    public Optional<Deposit> getDepositById(@PathVariable Long deposit_Id){
+        return depositService.getDepositById(deposit_Id);
+    }
+    @RequestMapping(value = "/deposits/{depositId}", method = RequestMethod.PUT)
+    public Deposit updateDeposit(@PathVariable Long deposit_Id, @RequestBody Deposit deposit){
+        depositService.updateDeposit(deposit_Id,deposit);
+        return depositService.getDepositById(deposit_Id).get();
+    }
+    @RequestMapping(value = "/deposits/{depositId}", method = RequestMethod.DELETE)
+    public void deleteDeposit(@PathVariable Long deposit_Id){
+        depositService.deleteDeposit(deposit_Id);
+    }
 }
--- src/main/java/com/cohort5/fullbankingapplicationfinal/service/DepositService.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/service/DepositService.java
@@ -1,4 +1,36 @@
 package com.cohort5.fullbankingapplicationfinal.service;
 
+import com.cohort5.fullbankingapplicationfinal.model.Account;
+import com.cohort5.fullbankingapplicationfinal.model.Deposit;
+import com.cohort5.fullbankingapplicationfinal.repository.AccountRepository;
+import com.cohort5.fullbankingapplicationfinal.repository.DepositRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class DepositService {
+    @Autowired
+    public DepositRepository depositRepository;
+    @Autowired
+    public AccountRepository accountRepository;
+
+    public Optional<Deposit> getDepositById(Long deposit_id){
+        return depositRepository.findById(deposit_id);
+    }
+    public void createDeposit(Long account_id, Deposit deposit){
+        depositRepository.save(deposit);
+        deposit.setAccount_Id(account_id);
+        Account account = accountRepository.getAccountById(account_id).get();
+        account.setBalance(deposit.getBalance() + deposit.getBalance());
+    }
+    public Deposit updateDeposit(Long account_Id, Deposit deposit){
+        deposit.setAccount_Id(account_Id);
+        return depositRepository.save(deposit);
+    }
+    public void deleteDeposit(Long deposit_id){
+        getDepositById(account_Id);
+        depositRepository.delete(getDepositById(deposit_id));
+    }
 }
