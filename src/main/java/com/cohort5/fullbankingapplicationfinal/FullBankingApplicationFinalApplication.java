37368ced27880dca35b80d345b13aef5c250bd10
billycropper <williamcropper@Billys-MacBook-Pro.local>
2019-06-06 11:02:10 -0400

    blank project

---
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/FullBankingApplicationFinalApplication.java
@@ -0,0 +1,13 @@
+package com.cohort5.fullbankingapplicationfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullBankingApplicationFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullBankingApplicationFinalApplication.class, args);
+	}
+
+}
