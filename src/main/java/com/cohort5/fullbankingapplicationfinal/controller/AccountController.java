55314e820c5200273df401f7ebb5b4a2f4a8a6ca
Chatyra <chatyrac@icloud.com>
2019-06-06 14:22:14 -0400

    commit

--- src/main/java/com/cohort5/fullbankingapplicationfinal/controller/AccountController.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/controller/AccountController.java
@@ -1,9 +1,27 @@
 package com.cohort5.fullbankingapplicationfinal.controller;
 
+import com.cohort5.fullbankingapplicationfinal.model.Account;
+import com.cohort5.fullbankingapplicationfinal.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
+    @Autowired
+    private AccountService accountService;
+
+    //get all accounts method
+    @RequestMapping(value = "/accounts", method = RequestMethod.GET)
+    public Iterable<Account> getAllAccounts()(@PathVariable Long account_id){
+        accountService.getAllAccounts();
+    }
+
+    //get account owner method
+    @RequestMapping(value = "/customers/{id}", method = RequestMethod.GET)
+    public
 
 
 }
--- src/main/java/com/cohort5/fullbankingapplicationfinal/service/AccountService.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/service/AccountService.java
@@ -2,16 +2,19 @@ package com.cohort5.fullbankingapplicationfinal.service;
 
 import com.cohort5.fullbankingapplicationfinal.model.*;
 import com.cohort5.fullbankingapplicationfinal.repository.AccountRepository;
+import com.cohort5.fullbankingapplicationfinal.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
+
 
 @Service
 public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
 
     //get all account method
     public Iterable<Account> getAllAccounts(){
@@ -25,7 +28,11 @@ public class AccountService {
 
     //create account method
     public Account createAccount(Long customer_id, Account account){
-        accountRepository.save(customer_id);
+        accountRepository.save(account);
+        account.set(customer_id);
+
+
+
 
     }
 
@@ -40,13 +47,18 @@ public class AccountService {
     }
 
     //get Account owner id method (customer)
+    //find the account by its ID... .get() is used to take the account out of the optional
+    //store account_id into a variable customer_id
     public Customer getAccountOwner (Long account_id){
-        account.get(customer_id);
-        accountRepository.findById(account_id);
+        Account account = accountRepository.findById(account_id).get();
+        Long customer_id = account.getCustomerId();
+        accountRepository.findById(customer_id);
+        return customer;
     }
 
     //get deposits by account method
     public Iterable<Deposit> getDepositsByAccount(Long account_id){
+        Account account = accountRepository.findById(account_id).get();
 
     }
 
@@ -59,8 +71,4 @@ public class AccountService {
     public Iterable<Bill> getBillsByAccount (Long account_id){
 
     }
-
-
-
-
 }
