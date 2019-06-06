c3aa2917a62b95ad163d0039cb3b2271cab4f2e5
billycropper <williamcropper@Billys-MacBook-Pro.local>
2019-06-06 13:51:22 -0400

    updated withdrawals

--- src/main/java/com/cohort5/fullbankingapplicationfinal/controller/WithdrawalController.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/controller/WithdrawalController.java
@@ -1,4 +1,40 @@
 package com.cohort5.fullbankingapplicationfinal.controller;
 
+import com.cohort5.fullbankingapplicationfinal.exception.HttpException;
+import com.cohort5.fullbankingapplicationfinal.model.Withdrawal;
+import com.cohort5.fullbankingapplicationfinal.repository.WithdrawalRepository;
+import com.cohort5.fullbankingapplicationfinal.service.WithdrawalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
 public class WithdrawalController {
+
+    @Autowired
+    private WithdrawalService withdrawalService;
+
+    @Autowired
+    private WithdrawalRepository withdrawalRepository;
+
+    @RequestMapping(path = "withdrawal/{withdrawalId}", method = RequestMethod.GET)
+    public Optional<Withdrawal> getWithdrawalById(@PathVariable Long id) {
+        Optional optional = withdrawalService.getWithdrawalById(id);
+        if(!optional.isPresent())
+            throw new HttpException(HttpStatus.NOT_FOUND, "Error fetching withdrawal");
+        if(optional.isPresent())
+            throw new HttpException(HttpStatus.OK, "Success");
+        return optional;
+    }
+
+    @RequestMapping(path = "/accounts/withdrawal/{withdrawalId}")
+    public Optional<Withdrawal> createWithdrawal(@PathVariable Long account_id, Withdrawal withdrawal) {
+        withdrawalService.createWithdrawal(account_id,withdrawal);
+        Optional<Withdrawal> optional = withdrawalService.getWithdrawalById(withdrawal.getId());
+        if(!optional.isPresent())
+            throw new HttpException(HttpStatus.NOT_FOUND)
+    }
+
 }
--- src/main/java/com/cohort5/fullbankingapplicationfinal/service/WithdrawalService.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/service/WithdrawalService.java
@@ -1,12 +1,16 @@
 package com.cohort5.fullbankingapplicationfinal.service;
 
+import com.cohort5.fullbankingapplicationfinal.model.Account;
 import com.cohort5.fullbankingapplicationfinal.model.Withdrawal;
 import com.cohort5.fullbankingapplicationfinal.repository.AccountRepository;
 import com.cohort5.fullbankingapplicationfinal.repository.WithdrawalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
+@Service
 public class WithdrawalService {
 
     @Autowired
@@ -17,6 +21,38 @@ public class WithdrawalService {
 
     public Optional<Withdrawal> getWithdrawalById(Long withdrawal_id) {
         return withdrawalRepository.findById(withdrawal_id);
+    }
+
+    public void createWithdrawal(Long account_id, Withdrawal withdrawal) {
+        withdrawalRepository.save(withdrawal);
+        withdrawal.setId(account_id);
+        Account account = accountRepository.findById(account_id).get();
+        Double newBalance = account.getBalance() - withdrawal.getAmount();
+        account.setBalance(newBalance);
+    }
+
+    public void updateWithdrawal(Long account_id, Withdrawal withdrawal) {
+        withdrawalRepository.save(withdrawal);
+        Account account = accountRepository.findById(account_id).get();
+        Double newBalance = account.getBalance() - withdrawal.getAmount();
+        account.setBalance(newBalance);
+    }
+
+    public void deleteWithdrawal(Long account_id, Long withdrawal_id) {
+        withdrawalRepository.deleteById(withdrawal_id);
+        Account account = accountRepository.findById(account_id).get();
+        Withdrawal withdrawal = withdrawalRepository.findById(withdrawal_id).get();
+        Double newBalance = account.getBalance() + withdrawal.getAmount();
+        account.setBalance(newBalance);
+    }
+
+    public ArrayList<Withdrawal> getWithdrawals() {
+        Iterable<Withdrawal> withdrawals = withdrawalRepository.findAll();
+        ArrayList<Withdrawal> all_withdrawals = new ArrayList<>();
+        for(Withdrawal withdrawal : withdrawals) {
+            all_withdrawals.add(withdrawal);
+        }
+        return all_withdrawals;
     }
 
 }
