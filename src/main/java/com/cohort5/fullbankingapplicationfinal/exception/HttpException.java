8637d28b15fdcf063f388871b1899e932cd1f7e0
Alberto Rodriguez <ararod9@gmail.com>
2019-06-06 14:11:09 -0400

    fourth commit

--- src/main/java/com/cohort5/fullbankingapplicationfinal/exception/HttpException.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/exception/HttpException.java
@@ -1,4 +1,23 @@
 package com.cohort5.fullbankingapplicationfinal.exception;
 
-public class HttpException {
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class HttpException extends RuntimeException{
+
+	private static final long serialVersionUID = 1l;
+
+	public HttpException(){}
+
+	public HttpException(String message){
+		super(message);
+	}
+
+	public HttpException(String message, Throwable cause){
+		super(message, cause);
+	}
+
+
 }
