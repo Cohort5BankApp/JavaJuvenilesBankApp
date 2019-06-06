79756857a86ec61382459ef9157dfee8ccb132e2
Chatyra <chatyrac@icloud.com>
2019-06-06 18:45:20 -0400

    rough draft commit

--- src/main/java/com/cohort5/fullbankingapplicationfinal/controller/AccountController.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/controller/AccountController.java
@@ -1,12 +1,11 @@
 package com.cohort5.fullbankingapplicationfinal.controller;
 
-import com.cohort5.fullbankingapplicationfinal.model.Account;
+import com.cohort5.fullbankingapplicationfinal.model.*;
 import com.cohort5.fullbankingapplicationfinal.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class AccountController {
@@ -15,13 +14,56 @@ public class AccountController {
 
     //get all accounts method
     @RequestMapping(value = "/accounts", method = RequestMethod.GET)
-    public Iterable<Account> getAllAccounts()(@PathVariable Long account_id){
-        accountService.getAllAccounts();
+    public Iterable<Account> getAllAccounts(){
+        return accountService.getAllAccounts();
     }
 
     //get account owner method
-    @RequestMapping(value = "/customers/{id}", method = RequestMethod.GET)
-    public
+    @RequestMapping(value = "/customers/{account_id}", method = RequestMethod.GET)
+    public Customer getAccountOwner(@PathVariable Long account_id){
+        return accountService.getAccountOwner(account_id);
+    }
 
+    //get bills by account method
+    @RequestMapping(value = "/accounts/{account_id}/bills", method = RequestMethod.GET)
+    public Iterable<Bill> getBillsByAccount(@PathVariable Long account_id){
+        return accountService.getBillsByAccount(account_id);
+    }
+
+    //get withdrawals by account method
+    @RequestMapping(value = "/accounts/{account_id}/withdrawals", method = RequestMethod.GET)
+    public Iterable<Withdrawal> getWithdrawalsByAccount(@PathVariable Long account_id){
+        return accountService.getWithdrawalsByAccount(account_id);
+    }
+
+    //get deposits by account method
+    @RequestMapping(value = "/accounts/{account_id}/deposits", method = RequestMethod.GET)
+    public Iterable<Deposit> getDepositsByAccount(@PathVariable Long account_id){
+        return accountService.getDepositsByAccount(account_id);
+    }
+
+    //create an account method
+    @RequestMapping(value = "/customers/{customers_id}/accounts", method = RequestMethod.POST)
+    public Account createAccount(@RequestBody Account account, @PathVariable Long customer_id ){
+        return accountService.createAccount(customer_id, account);
+    }
+
+    //get account by id method
+    @RequestMapping(value = "/accounts/{account_id}", method = RequestMethod.GET)
+    public Optional<Account> getAccountById(@PathVariable Long account_id){
+        return accountService.getAccountById(account_id);
+    }
+
+    //update an account method
+    @RequestMapping(value = "/accounts/{account_id}", method = RequestMethod.PUT)
+    public Account updateAccount(@RequestBody Account account){
+        return accountService.updateAccount(account);
+    }
+
+    //delete an account method
+    @RequestMapping(value = "/accounts/{account_id}", method = RequestMethod.DELETE)
+    public void deleteAccount(@PathVariable Long account_id){
+        accountService.deleteAccount(account_id);
+    }
 
 }
--- src/main/java/com/cohort5/fullbankingapplicationfinal/service/AccountService.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/service/AccountService.java
@@ -1,11 +1,11 @@
 package com.cohort5.fullbankingapplicationfinal.service;
 
 import com.cohort5.fullbankingapplicationfinal.model.*;
-import com.cohort5.fullbankingapplicationfinal.repository.AccountRepository;
-import com.cohort5.fullbankingapplicationfinal.repository.CustomerRepository;
+import com.cohort5.fullbankingapplicationfinal.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 
@@ -15,6 +15,12 @@ public class AccountService {
     private AccountRepository accountRepository;
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private DepositRepository depositRepository;
+    @Autowired
+    private WithdrawalRepository withdrawalRepository;
+    @Autowired
+    private BillRepository billRepository;
 
     //get all account method
     public Iterable<Account> getAllAccounts(){
@@ -29,11 +35,8 @@ public class AccountService {
     //create account method
     public Account createAccount(Long customer_id, Account account){
         accountRepository.save(account);
-        account.set(customer_id);
-
-
-
-
+        account.setCustomer_id(customer_id);
+        return account;
     }
 
     //update an account method
@@ -49,26 +52,51 @@ public class AccountService {
     //get Account owner id method (customer)
     //find the account by its ID... .get() is used to take the account out of the optional
     //store account_id into a variable customer_id
+    //find the customer that is associated with that account_id and then return that customer
     public Customer getAccountOwner (Long account_id){
         Account account = accountRepository.findById(account_id).get();
-        Long customer_id = account.getCustomerId();
-        accountRepository.findById(customer_id);
+        Long customer_id = account.getCustomer_id();
+        Customer customer = customerRepository.findById(customer_id).get();
         return customer;
     }
 
     //get deposits by account method
+    //find the account by its ID and store it into the account variable
+    //find all of the deposits and store it into a variable
+    //take the deposits grabbed and the account_id grabbed and compare the two
+    //if the two are
     public Iterable<Deposit> getDepositsByAccount(Long account_id){
         Account account = accountRepository.findById(account_id).get();
-
+        Iterable<Deposit> deposit = depositRepository.findAll();
+        ArrayList<Deposit> list = new ArrayList<>();
+        for (Deposit deposits : deposit){
+            if(deposit.getAccount_Id() == account_id)
+                list.add(deposits);
+        }
+        return list;
     }
 
     //get withdrawals by account method
-    public Iterable<Withdrawal> getWithdrawalsByAccount (Long account_id){
-
+    public Iterable<Withdrawal> getWithdrawalsByAccount (Long account_id) {
+        Account account = accountRepository.findById(account_id).get();
+        Iterable<Withdrawal> withdrawal = withdrawalRepository.findAll();
+        ArrayList<Withdrawal> list = new ArrayList<>();
+        for (Withdrawal withdrawals : withdrawal) {
+            if (withdrawal.getAccount_Id() == account_id)
+                list.add(withdrawals);
+        }
+        return list;
     }
 
     //get bills by account method
     public Iterable<Bill> getBillsByAccount (Long account_id){
-
+        Account account = accountRepository.findById(account_id).get();
+        Iterable<Bill> bill = billRepository.findAll();
+        ArrayList<Bill> list = new ArrayList<>();
+        for (Bill bills : bill){
+            if(bill.getAccount_Id() == account_id)
+                list.add(bills);
+        }
+        return list;
     }
 }
