99b04251f36c9c5b79d00de807a3b2d024fcef4e
billycropper <williamcropper@Billys-MacBook-Pro.local>
2019-06-06 13:17:15 -0400

    commit

--- src/main/java/com/cohort5/fullbankingapplicationfinal/service/WithdrawalService.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/service/WithdrawalService.java
@@ -1,4 +1,22 @@
 package com.cohort5.fullbankingapplicationfinal.service;
 
+import com.cohort5.fullbankingapplicationfinal.model.Withdrawal;
+import com.cohort5.fullbankingapplicationfinal.repository.AccountRepository;
+import com.cohort5.fullbankingapplicationfinal.repository.WithdrawalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
 public class WithdrawalService {
+
+    @Autowired
+    private WithdrawalRepository withdrawalRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Optional<Withdrawal> getWithdrawalById(Long withdrawal_id) {
+        return withdrawalRepository.findById(withdrawal_id);
+    }
+
 }
