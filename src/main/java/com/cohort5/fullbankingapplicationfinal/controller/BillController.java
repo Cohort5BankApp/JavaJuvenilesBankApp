ece85f2df8eee0e5bbfc35e44a8a3deedf441a1f
jjackson053 <ljjackson255@gmail.com>
2019-06-10 14:40:17 -0400

    update

--- src/main/java/com/cohort5/fullbankingapplicationfinal/controller/BillController.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/controller/BillController.java
@@ -8,75 +8,85 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-@RequestMapping("/bills")
 public class BillController {
 
     @Autowired
     private BillService billService;
 
-    @RequestMapping(value = "/{billId}", method = RequestMethod.GET)
+    @RequestMapping(value = "/bills/{bill_id}", method = RequestMethod.GET)
     public Optional<Bill> getBill (@PathVariable Long bill_id){
        Optional<Bill> optionalBill = billService.getBillById(bill_id);
-
-       if(!optionalBill.isPresent())
-           throw new HttpException(HttpStatus.NOT_FOUND, "error fetching bill");
-       if(optionalBill.isPresent())
-           throw new HttpException(HttpStatus.OK, "Successful");
+//
+//       if(!optionalBill.isPresent())
+//           throw new HttpException(HttpStatus.NOT_FOUND, "error fetching bill");
+//       if(optionalBill.isPresent())
+//           throw new HttpException(HttpStatus.OK, "Successful");
 
         return billService.getBillById(bill_id);
 
     }
 
-    @RequestMapping(value = "/createBills", method = RequestMethod.POST)
+    @RequestMapping(value = "/accounts/{account_Id}/bills", method = RequestMethod.POST)
     public void createBill(@RequestBody Bill bill, @PathVariable Long account_Id ){
         billService.createBill(account_Id,bill);
         Optional<Bill> optionalBill = billService.getBillById(bill.getId());
-         if (!optionalBill.isPresent())
-             throw new HttpException(HttpStatus.NOT_FOUND, "error fetching bill");
-
-         if (bill != null)
-
-         if (optionalBill.isPresent())
-
-             throw new HttpException(HttpStatus.OK, "Successful");
+//         if (!optionalBill.isPresent())
+//             throw new HttpException(HttpStatus.NOT_FOUND, "error fetching bill");
+//
+//         if (bill != null)
+//
+//         if (optionalBill.isPresent())
+//
+//             throw new HttpException(HttpStatus.OK, "Successful");
 
     }
 
-    @RequestMapping(value = "/{billId}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/bills/{bill_id}", method = RequestMethod.PUT)
     public void updateBill(@RequestBody Bill bill, @PathVariable Long bill_id) {
         /*TODO: adding code to get account id*/
         Long account_Id = bill.getAccount_id();
         /* TODO: End of edit */
         billService.updateBill(bill, account_Id);
         Bill optionalBill = billService.getBillById(bill_id).get();
-        if (optionalBill.toString() != bill.toString())
-            throw new HttpException(HttpStatus.NOT_FOUND, "error updating bill");
+//        if (optionalBill.toString() != bill.toString())
+//            throw new HttpException(HttpStatus.NOT_FOUND, "error updating bill");
 
         billService.updateBill(bill,account_Id);
-
-        if (optionalBill.toString() == bill.toString())
-            throw new HttpException(HttpStatus.OK, "Successful");
+//
+//        if (optionalBill.toString() == bill.toString())
+//            throw new HttpException(HttpStatus.OK, "Successful");
     }
 
-    @RequestMapping(value = "/{billId}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/bills/{bill_Id}", method = RequestMethod.DELETE)
     public void deleteBill(@PathVariable Long bill_Id){
         /*TODO: adding code to get account id*/
         Bill bill = billService.getBillById(bill_Id).get();
         Long account_Id = bill.getAccount_id();
         /* TODO: End of edit */
         billService.deleteBill(bill_Id, account_Id);
-       Optional<Bill> optionalBill = billService.getBillById(bill_Id);
-       if(optionalBill.isPresent())
-           throw new HttpException(HttpStatus.NOT_FOUND, "error deleting bill");
-       if(!optionalBill.isPresent())
-           throw new HttpException(HttpStatus.OK, "Successful");
+//       Optional<Bill> optionalBill = billService.getBillById(bill_Id);
+//       if(optionalBill.isPresent())
+//           throw new HttpException(HttpStatus.NOT_FOUND, "error deleting bill");
+//       if(!optionalBill.isPresent())
+//           throw new HttpException(HttpStatus.OK, "Successful");
         billService.deleteBill(bill_Id,account_Id);
     }
 
+//@RequestMapping(value = "/accounts/{account_Id}/bills", method = RequestMethod.GET)
+//    public void getAllBills (@PathVariable Long account_Id){
+//   Iterable<Bill> iterablebills = billService.getAllBills();
+//
+////   if (iterablebills.toString().isEmpty())
+////       throw new HttpException(HttpStatus.NOT_FOUND, "error getting bills");
+////  if(!iterablebills.toString().isEmpty())
+////      throw new HttpException(HttpStatus.OK, "Successful");
+//
+//}
 
 
 }
