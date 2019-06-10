9b90d905975ff69766a7bd66d2c5b1aec96d1031
Chatyra <chatyrac@icloud.com>
2019-06-10 14:26:21 -0400

    commit

--- src/main/java/com/cohort5/fullbankingapplicationfinal/controller/AccountController.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/controller/AccountController.java
@@ -16,6 +16,18 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
 
+    //inside is what is happening when there is an exception error
+    @GetMapping("/age")
+    ResponseEntity<String> age(@RequestParam("yearOfBirth") int yearOfBirth) {
+        if (isInFuture(yearOfBirth)) {
+            return ResponseEntity.badRequest()
+                    .body("Year of birth cannot be in the future");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Your age is " + calculateAge(yearOfBirth));
+    }
+
     //get all accounts method
     @RequestMapping(value = "/accounts", method = RequestMethod.GET)
     public Iterable<Account> getAllAccounts() {
