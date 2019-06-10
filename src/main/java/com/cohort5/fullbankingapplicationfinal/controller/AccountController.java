3348143343ec30cb3e96832ea7ce5b25c9dfe685
Chatyra <chatyrac@icloud.com>
2019-06-10 15:22:02 -0400

    commiting

--- src/main/java/com/cohort5/fullbankingapplicationfinal/controller/AccountController.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/controller/AccountController.java
@@ -21,10 +21,6 @@ public class AccountController {
     @RequestMapping(value = "/accounts", method = RequestMethod.GET)
     public Iterable<Account> getAllAccounts() {
         ArrayList<Account> arrayList = accountService.getAllAccounts();
-//        if (arrayList.size() < 1)
-//            throw new HttpException(HttpStatus.NOT_FOUND, "error fetching accounts");
-//        if (arrayList.size() > 0)
-//            throw new HttpException(HttpStatus.OK, "Success");
         return accountService.getAllAccounts();
     }
 
@@ -33,10 +29,6 @@ public class AccountController {
     @RequestMapping(value = "/accounts/{accountId}/customer", method = RequestMethod.GET)
     public Optional<Customer> getAccountOwner(@PathVariable Long accountId) {
         Optional<Customer> customer = Optional.of(accountService.getAccountOwner(accountId));
-//        if (!customer.isPresent())
-//            throw new HttpException(HttpStatus.NOT_FOUND, "error fetching account owner for account id: " + accountId);
-//        if (customer.isPresent())
-//            throw new HttpException(HttpStatus.OK, "Success");
         return customer;
     }
 
@@ -44,10 +36,6 @@ public class AccountController {
     @RequestMapping(value = "/accounts/{accountId}/bills", method = RequestMethod.GET)
     public Iterable<Bill> getBillsByAccount(@PathVariable Long accountId){
         ArrayList<Bill> arrayList = accountService.getBillsByAccount(accountId);
-//        if(arrayList.size() < 1)
-//            throw new HttpException(HttpStatus.NOT_FOUND, "error fetching bills with id");
-//        if(arrayList.size() > 0)
-//            throw new HttpException(HttpStatus.OK, "Success");
             return accountService.getBillsByAccount(accountId);
     }
 
@@ -55,10 +43,6 @@ public class AccountController {
     @RequestMapping(value = "/accounts/accountId}/withdrawals", method = RequestMethod.GET)
     public Iterable<Withdrawal> getWithdrawalsByAccount(@PathVariable Long accountId){
       ArrayList<Withdrawal> arrayList = accountService.getWithdrawalsByAccount(accountId);
-//      if(arrayList.size() < 1)
-//          throw new HttpException(HttpStatus.NOT_FOUND, "error fetching withdrawals with id");
-//      if(arrayList.size() > 0)
-//          throw new HttpException(HttpStatus.OK, "Success");
           return accountService.getWithdrawalsByAccount(accountId);
     }
 
@@ -66,41 +50,22 @@ public class AccountController {
     @RequestMapping(value = "/accounts/{accountId}/deposits", method = RequestMethod.GET)
     public Iterable<Deposit> getDepositsByAccount(@PathVariable Long accountId){
         ArrayList<Deposit> arrayList = accountService.getDepositsByAccount(accountId);
-//        if(arrayList.size() < 1)
-//            throw new HttpException(HttpStatus.NOT_FOUND, " error fetching deposits with id");
-//        if(arrayList.size() > 0)
-//            throw new HttpException(HttpStatus.OK, "Success");
             return accountService.getDepositsByAccount(accountId);
     }
 
     //create an account method
-    @RequestMapping(value = "/createAccount", method = RequestMethod.POST)
-    public Account createAccount(@RequestBody Account account){
-        Account account1 = accountService.createAccount(account);
-        Long account_id = account1.getAccount_id();
-        Optional<Account> optionalAccount = accountService.getAccountById(account_id);
-}
     @RequestMapping(value = "/customers/{customerId}/accounts", method = RequestMethod.POST)
-    public Account createAccount(@RequestBody Account account, @PathVariable Long customerId ) {
-        Account account1 = accountService.createAccount(customerId, account);
-//        Long account_id = account1.getAccount_id();
-//        Optional<Account> optionalAccount = accountService.getAccountById(account_id);
-//        if(!optionalAccount.isPresent())
-//            throw new HttpException(HttpStatus.NOT_FOUND, "error creating customers account");
-//        if(optionalAccount.isPresent())
-//            throw new HttpException(HttpStatus.OK, "Success creating account id: " + optionalAccount.get().getAccount_id())
-            return account1;
-
+    public Optional<Account> createAccount(@RequestBody Account account, @PathVariable Long customerId){
+    Account account1 = accountService.createAccount(account);
+    Long account_id = account1.getAccount_id();
+    Optional<Account> optionalAccount = accountService.getAccountById(account_id);
+        return optionalAccount;
     }
 
     //get account by id method
     @RequestMapping(value = "/accounts/{accountId}", method = RequestMethod.GET)
     public Optional<Account> getAccountById(@PathVariable Long accountId) {
         Optional<Account> optionalAccount = accountService.getAccountById(accountId);
-//        if(!optionalAccount.isPresent())
-//            throw new HttpException(HttpStatus.NOT_FOUND, "error fetching account");
-//        if(optionalAccount.isPresent())
-//            throw new HttpException(HttpStatus.OK, "Success");
             return accountService.getAccountById(accountId);
         }
 
@@ -109,11 +74,6 @@ public class AccountController {
     public Account updateAccount(@RequestBody Account account, @PathVariable Long accountId){
         accountService.updateAccount(account);
         Account account1 = accountService.getAccountById(account.getAccount_id()).get();
-//        /*TODO: changing Http Exception 'if' params */
-//        if (account1.toString() != account.toString())
-//            throw new HttpException(HttpStatus.NOT_FOUND, "Error");
-//        if (account1.toString() == account.toString())
-//            throw new HttpException(HttpStatus.OK, "Customer Account Updated");
             return account1;
     }
 
@@ -121,11 +81,6 @@ public class AccountController {
     @RequestMapping(value = "/accounts/{accountId}", method = RequestMethod.DELETE)
     public void deleteAccount(@PathVariable Long accountId){
         Optional<Account> optionalAccount = accountService.getAccountById(accountId);
-//        /* TODO: Switching around 'if' params */
-//        if(optionalAccount.isPresent())
-//            throw new HttpException(HttpStatus.OK, "error deleting account");
-//        if(!optionalAccount.isPresent())
-//        throw new HttpException(HttpStatus.BAD_REQUEST, "Account successfully deleted");
        accountService.deleteAccount(accountId);
     }
 
