4c9b3d8214801fab0beb7923f872fd13c093f859
Chatyra <chatyrac@icloud.com>
2019-06-11 12:03:19 -0400

    message commit

--- src/main/java/com/cohort5/fullbankingapplicationfinal/controller/AccountController.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/controller/AccountController.java
@@ -18,13 +18,14 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
 
-    
     //get all accounts method
     //Iterable<Account>
     @RequestMapping(value = "/accounts", method = RequestMethod.GET)
     public ResponseEntity<?> getAllAccounts() {
-        ArrayList<Account> arrayList = accountService.getAllAccounts();
-        return accountService.getAllAccounts();
+        accountService.getAllAccounts();
+//        Iterable<Account> accounts = accountService.getAllAccounts();
+        Message message = new Message(HttpStatus.CREATED.value(), "Success", accountService.getAllAccounts());
+        return new ResponseEntity<>(message, HttpStatus.CREATED);
     }
 
     //get account owner method
@@ -32,67 +33,65 @@ public class AccountController {
     //option.of throws the NullPointerException if the passed parameter is null
     @RequestMapping(value = "/accounts/{accountId}/customer", method = RequestMethod.GET)
     public ResponseEntity<?> getAccountOwner(@PathVariable Long accountId) {
-        Optional<Customer> customer = Optional.of(accountService.getAccountOwner(accountId));
-        return customer;
+//        Optional<Customer> customer = Optional.of(accountService.getAccountOwner(accountId));
+        Message message = new Message(HttpStatus.OK.value(), "Success", accountService.getAccountOwner(accountId));
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     //get bills by account method
     //Iterable<Bill>
     @RequestMapping(value = "/accounts/{accountId}/bills", method = RequestMethod.GET)
     public ResponseEntity<?> getBillsByAccount(@PathVariable Long accountId){
-        ArrayList<Bill> arrayList = accountService.getBillsByAccount(accountId);
-            return accountService.getBillsByAccount(accountId);
+        Message message = new Message(HttpStatus.OK.value(), "Success", accountService.getBillsByAccount(accountId));
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     //get withdrawals by account method
     //Iterable<Withdrawal>
-    @RequestMapping(value = "/accounts/accountId}/withdrawals", method = RequestMethod.GET)
+    @RequestMapping(value = "/accounts/{accountId}/withdrawals", method = RequestMethod.GET)
     public ResponseEntity<?> getWithdrawalsByAccount(@PathVariable Long accountId){
-      ArrayList<Withdrawal> arrayList = accountService.getWithdrawalsByAccount(accountId);
-          return accountService.getWithdrawalsByAccount(accountId);
+      Message message = new Message(HttpStatus.OK.value(), "Success", accountService.getWithdrawalsByAccount(accountId));
+          return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     //get deposits by account method
     //Iterable<Deposits>
     @RequestMapping(value = "/accounts/{accountId}/deposits", method = RequestMethod.GET)
     public ResponseEntity<?> getDepositsByAccount(@PathVariable Long accountId){
-        ArrayList<Deposit> arrayList = accountService.getDepositsByAccount(accountId);
-            return accountService.getDepositsByAccount(accountId);
+        Message message = new Message(HttpStatus.OK.value(), "Success", accountService.getDepositsByAccount(accountId));
+            return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     //create an account method check
     //Optional<Account>
     @RequestMapping(value = "/customers/{customerId}/accounts", method = RequestMethod.POST)
     public ResponseEntity<?> createAccount(@RequestBody Account account, @PathVariable Long customerId){
-    Account account1 = accountService.createAccount(account);
-    Long account_id = account1.getAccount_id();
-    Optional<Account> optionalAccount = accountService.getAccountById(account_id);
-        return optionalAccount;
+    Message message = new Message(HttpStatus.OK.value(),"Success", accountService.createAccount(account));
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     //get account by id method
     //Optional<Account>
     @RequestMapping(value = "/accounts/{accountId}", method = RequestMethod.GET)
     public ResponseEntity<?> getAccountById(@PathVariable Long accountId) {
-        Optional<Account> optionalAccount = accountService.getAccountById(accountId);
-            return accountService.getAccountById(accountId);
-        }
+        Message message = new Message(HttpStatus.OK.value(), "Success", accountService.getAccountById(accountId));
+            return new ResponseEntity<>(message, HttpStatus.OK);
+    }
 
     //update an account method
     //Account
     @RequestMapping(value = "/accounts/{accountId}", method = RequestMethod.PUT)
     public ResponseEntity<?> updateAccount(@RequestBody Account account, @PathVariable Long accountId){
-        accountService.updateAccount(account);
-        Account account1 = accountService.getAccountById(account.getAccount_id()).get();
-            return account1;
+        Message message = new Message(HttpStatus.OK.value(), "Success", accountService.updateAccount(account));
+            return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     //delete an account method
     //void
     @RequestMapping(value = "/accounts/{accountId}", method = RequestMethod.DELETE)
     public ResponseEntity<?> deleteAccount(@PathVariable Long accountId){
-        Optional<Account> optionalAccount = accountService.getAccountById(accountId);
-       accountService.deleteAccount(accountId);
+        accountService.deleteAccount(accountId);
+       Message message = new Message(HttpStatus.OK.value(), "Success");
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
-
 }
