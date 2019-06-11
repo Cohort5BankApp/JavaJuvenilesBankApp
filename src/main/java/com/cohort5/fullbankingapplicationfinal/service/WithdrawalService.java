670f0f5df52126b27f7949416092f6fc5799e3c5
billycropper <williamcropper@Billys-MacBook-Pro.local>
2019-06-11 11:19:31 -0400

    final commit

--- src/main/java/com/cohort5/fullbankingapplicationfinal/service/WithdrawalService.java
+++ src/main/java/com/cohort5/fullbankingapplicationfinal/service/WithdrawalService.java
@@ -7,42 +7,49 @@ import com.cohort5.fullbankingapplicationfinal.repository.WithdrawalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Service
 public class WithdrawalService {
 
     @Autowired
-    private WithdrawalRepository withdrawalRepository;
+    WithdrawalRepository withdrawalRepository;
 
     @Autowired
-    private AccountRepository accountRepository;
+    AccountRepository accountRepository;
+
+    @Autowired
+    AccountService accountService;
 
     public Optional<Withdrawal> getWithdrawalById(Long id) {
         return withdrawalRepository.findById(id);
     }
 
-    public Withdrawal createWithdrawal(Long account_id, Withdrawal withdrawal) {
-        return withdrawalRepository.save(withdrawal);
-//        withdrawal.setId(account_id);
-//        Account account = accountRepository.findById(account_id).get();
-//        Double newBalance = account.getBalance() - withdrawal.getAmount();
-//        account.setBalance(newBalance);
-    }
-
-    public void updateWithdrawal(Long account_id, Withdrawal withdrawal) {
-        withdrawalRepository.save(withdrawal);
+    public void createWithdrawal(Long account_id, Withdrawal withdrawal) {
+        withdrawal.setAccount_id(account_id);
         Account account = accountRepository.findById(account_id).get();
         Double newBalance = account.getBalance() - withdrawal.getAmount();
         account.setBalance(newBalance);
+        withdrawalRepository.save(withdrawal);
+        //return withdrawalRepository.findById(withdrawal.getId());
     }
 
-    public void deleteWithdrawal(Long account_id, Long withdrawal_id) {
-        withdrawalRepository.deleteById(withdrawal_id);
-        Account account = accountRepository.findById(account_id).get();
+    public void updateWithdrawal(Withdrawal withdrawal) {
+        Long account_id = withdrawal.getAccount_id();
+        Account account = accountService.getAccountById(account_id).get();
+        Withdrawal withdrawal1 = withdrawalRepository.findById(withdrawal.getId()).get();
+        Double newBalance = (account.getBalance() + withdrawal1.getAmount()) - withdrawal.getAmount();
+        account.setBalance(newBalance);
+        withdrawalRepository.save(withdrawal);
+    }
+
+    public void deleteWithdrawal(Long withdrawal_id, Long account_id) {
+        Account account = accountService.getAccountById(account_id).get();
         Withdrawal withdrawal = withdrawalRepository.findById(withdrawal_id).get();
         Double newBalance = account.getBalance() + withdrawal.getAmount();
         account.setBalance(newBalance);
+        withdrawalRepository.deleteById(withdrawal_id);
     }
 
 //    Possibly do not need method, saving for future
